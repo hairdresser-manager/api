1e7e73ec294aafe5ee3a218ccc18da97e3e65824
andre <gombkaa@gmail.com>
2021-05-17 21:51:38 +0200

    Setup ApplicationCore unit tests project with xUnit

---
+++ HairdresserManager/tests/ApplicationCore.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace ApplicationCore.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
